da8ddfab5c9ede890b1551bd1786c96727d3ba71
Guido Abel Sanchez <sanchezguidorl@gmail.com>
2025-09-29 00:45:34 -0300

    Se completa implementación de analitycsw

--- app/ObservaBook.tsx
+++ app/ObservaBook.tsx
@@ -3,7 +3,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { motion } from "framer-motion";
-import FloatingProgressBar from "./FloatingProgressBar";
 
 export default function ObservaBook() {
   return (
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Analytics } from "@vercel/analytics/next";
 import ObservaBook from "./ObservaBook";
 
 export default function Home() {
   return (
     <main>
       <ObservaBook />
+      <Analytics />
     </main>
   );
 }
